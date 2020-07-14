16e7783fb19731351152367bb83cde1b3f8d8501
ермек <ermek@MacBook-Pro-ermek.local>
2020-07-14 16:39:41 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 20-45;
+        int temperature = 10;
+        boolean isRaining = false;
+
+        if (temperature < -20 || temperature < 30);
+            System.out.println(" Можно идти гулять");
+
+        if (temperature < 28) {
+            System.out.println(" Можно идти гулять");
+        }
+    }
+}
